af036a9b3360441d602b64ab7cdb9c642e1d004c
Drisya-G <111036377+Drisya-G@users.noreply.github.com>
2022-12-30 12:37:35 +0530

    netflix clone

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import Row from './components/Row';
+import requests from './request';
+import Banner from './components/Banner';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Nav/>
+        
+      </header>
+      <Banner  fetchUrl={requests.fetchNetflixOriginals} />
+      <Row 
+      isLargeRow={true}
+      title="Netflix Originals" 
+      fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+
+    </div>
+  );
+}
+
+export default App;
